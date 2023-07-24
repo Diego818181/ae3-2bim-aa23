0f464857c12d1b63a04e375c2d03b3515e5ca7b9
Diego <sarmiento.df@gmail.com>
2023-07-24 11:37:16 -0500

    ajuste de presentación

--- Deber.py
+++ Deber.py
@@ -10,30 +10,49 @@ class Ui_MainWindow(object):
         MainWindow.resize(689, 514)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+        # Campos de texto
         self.nombre = QtWidgets.QLineEdit(self.centralwidget)
-        self.nombre.setGeometry(QtCore.QRect(140, 80, 113, 23))
+        self.nombre.setGeometry(QtCore.QRect(140, 30, 113, 23))
         self.nombre.setObjectName("nombre")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(70, 80, 54, 15))
+        self.label.setGeometry(QtCore.QRect(70, 30, 54, 15))
         self.label.setObjectName("label")
         self.nota = QtWidgets.QLineEdit(self.centralwidget)
-        self.nota.setGeometry(QtCore.QRect(140, 150, 113, 23))
+        self.nota.setGeometry(QtCore.QRect(140, 70, 113, 23))
         self.nota.setText("")
         self.nota.setObjectName("nota")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(70, 150, 54, 15))
+        self.label_2.setGeometry(QtCore.QRect(70, 70, 54, 15))
         self.label_2.setObjectName("label_2")
+
+        # Botones
         self.guardar = QtWidgets.QPushButton(self.centralwidget)
-        self.guardar.setGeometry(QtCore.QRect(70, 210, 211, 21))
+        self.guardar.setGeometry(QtCore.QRect(70, 120, 211, 21))
         self.guardar.setObjectName("guardar")
         self.actualizar = QtWidgets.QPushButton(self.centralwidget)
-        self.actualizar.setGeometry(QtCore.QRect(70, 262, 211, 21))
+        self.actualizar.setGeometry(QtCore.QRect(70, 160, 211, 21))
         self.actualizar.setObjectName("actualizar")
+
+        # Grilla con nombres de columnas
         self.listaCalificaciones = QtWidgets.QTableWidget(self.centralwidget)
-        self.listaCalificaciones.setGeometry(QtCore.QRect(370, 70, 256, 192))
+        self.listaCalificaciones.setGeometry(QtCore.QRect(70, 200, 551, 261))
         self.listaCalificaciones.setObjectName("listaCalificaciones")
-        self.listaCalificaciones.setColumnCount(0)
+        self.listaCalificaciones.setColumnCount(3)
         self.listaCalificaciones.setRowCount(0)
+        self.listaCalificaciones.setHorizontalHeaderLabels(["Nombre", "Nota", "Fecha"])
+
+        # Organización de elementos
+        self.layout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.nombre)
+        self.layout.addWidget(self.label_2)
+        self.layout.addWidget(self.nota)
+        self.layout.addWidget(self.guardar)
+        self.layout.addWidget(self.actualizar)
+        self.layout.addWidget(self.listaCalificaciones)
+        self.centralwidget.setLayout(self.layout)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 689, 20))
@@ -83,19 +102,13 @@ class Ui_MainWindow(object):
         cadena_consulta_sql = "SELECT * from Calificacion"
         cursor.execute(cadena_consulta_sql)
         informacion = cursor.fetchall()
-        database_table_column_count = 3
-        self.listaCalificaciones.setColumnCount(database_table_column_count)
-        numero_filas = len(informacion)
-        self.listaCalificaciones.setRowCount(numero_filas)
-        for j in range(numero_filas):
-            valor = informacion[j]
-            for i in range(0, len(valor)):
-                elemento = valor[i]
-                if i == 2:  # Convertir la fecha al formato adecuado
-                    elemento = datetime.strptime(elemento, "%Y-%m-%d").strftime("%d-%m-%Y")
-                elemento = str(elemento)
-                nuevo_registro = QtWidgets.QTableWidgetItem(elemento)
-                self.listaCalificaciones.setItem(j, i, nuevo_registro)
+        self.listaCalificaciones.setRowCount(0)
+        for row_num, row_data in enumerate(informacion):
+            self.listaCalificaciones.insertRow(row_num)
+            for col_num, col_data in enumerate(row_data):
+                if col_num == 2:  # Convertir la fecha al formato adecuado
+                    col_data = datetime.strptime(col_data, "%Y-%m-%d").strftime("%d-%m-%Y")
+                self.listaCalificaciones.setItem(row_num, col_num, QtWidgets.QTableWidgetItem(str(col_data)))
 
 if __name__ == "__main__":
     import sys
